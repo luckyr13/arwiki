6d90338d548aec191f2c15e6421b54e33fe0c95c
technodromex <ricardo.guzmantorres@owasp.org>
2021-03-23 20:26:40 -0600

    Fix link path for argo deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import { SearchComponent } from './search/search.component';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
-    return new TranslateHttpLoader(http);
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 @NgModule({
