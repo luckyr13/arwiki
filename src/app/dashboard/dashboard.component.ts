5d7a9e31df2c6b946b1e279c6ea3334bcfb2a076
technodromex <ricardo.guzmantorres@owasp.org>
2021-04-08 22:08:30 -0500

    Update dashboard component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import { ArweaveService } from '../auth/arweave.service';
+import { Observable, Subscription, EMPTY } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
+declare const window: any;
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +12,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+	mainAddress: string = this._auth.getMainAddressSnapshot();
+	balance: Observable<string> = this._arweave.getAccountBalance(this.mainAddress);
+	
+  loading: boolean = false;
+  txmessage: string = '';
+  lastTransactionID: Observable<string> = this._arweave.getLastTransactionID(this.mainAddress);
+  
 
-  constructor() { }
+  constructor(
+  	private _router: Router,
+  	private _snackBar: MatSnackBar,
+  	private _arweave: ArweaveService,
+    private _auth: AuthService,
+  ) { }
 
   ngOnInit(): void {
+  	this.loading = true;
+  
+  	// Fetch data to display
+  	// this.loading is updated to false on success
+  	this.getUserInfo();
+
+  }
+
+  /*
+  *	Custom snackbar message
+  */
+  message(msg: string, panelClass: string = '', verticalPosition: any = undefined) {
+    this._snackBar.open(msg, 'X', {
+      duration: 8000,
+      horizontalPosition: 'center',
+      verticalPosition: verticalPosition,
+      panelClass: panelClass
+    });
+  }
+
+
+  /*
+  *	@dev Get user info
+  */
+  getUserInfo() {
+
+    this.loading = false;
+  }
+
+  /*
+  *	@dev Destroy subscriptions
+  */
+  ngOnDestroy() {
+  }
+
+  /*
+  * @dev Reload page
+  */
+  reload() {
+    window.location.reload();
   }
 
 }
