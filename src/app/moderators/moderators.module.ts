8be32e94e2441d922e74f94dfe3762bd633ffaec
technodromex <ricardo.guzmantorres@owasp.org>
2021-04-12 11:45:24 -0500

    Add menu for components in moderators module

--- src/app/moderators/moderators.module.ts
+++ src/app/moderators/moderators.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ModeratorsRoutingModule } from './moderators-routing.module';
 import { PendingListComponent } from './pending-list/pending-list.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
-  declarations: [PendingListComponent],
+  declarations: [PendingListComponent, MenuComponent],
   imports: [
     CommonModule,
     ModeratorsRoutingModule
