336db61c3af59277bea174fc30f86c5310f2a7e7
technodromex <ricardo.guzmantorres@owasp.org>
2021-04-16 00:17:27 -0500

    Update category detail

--- src/app/category/view-detail/view-detail.component.ts
+++ src/app/category/view-detail/view-detail.component.ts
@@ -1,14 +1,88 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ArwikiQuery } from '../../core/arwiki-query';
+import { ArweaveService } from '../../core/arweave.service';
+import { Subscription } from 'rxjs';
+import { ArwikiSettingsContract } from '../../arwiki-contracts/arwiki-settings';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { getVerification } from "arverify";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './view-detail.component.html',
   styleUrls: ['./view-detail.component.scss']
 })
 export class ViewDetailComponent implements OnInit {
+  arwikiQuery: ArwikiQuery|null = null;
+  pagesSubscription: Subscription = Subscription.EMPTY;
+  loadingPages: boolean = false;
+  category: string = '';
+  pages: any[] = [];
+  routeLang: string = '';
 
-  constructor() { }
+  constructor(
+  	private _arweave: ArweaveService,
+  	private _settingsContract: ArwikiSettingsContract,
+  	private _snackBar: MatSnackBar,
+  	private _route: ActivatedRoute
+ 	) { }
 
   ngOnInit(): void {
+    // Init ardb instance
+    this.arwikiQuery = new ArwikiQuery(this._arweave.arweave);
+  	this.category = this._route.snapshot.paramMap.get('category')!;
+    this.routeLang = this._route.snapshot.paramMap.get('lang')!;
+    this.loadingPages = true;
+
+    this.pagesSubscription = this.arwikiQuery.getPagesByCategory(
+    	this.category,
+    	this._settingsContract
+    ).subscribe({
+    	next: (pages) => {
+
+    		this.pages = pages;
+    		this.loadingPages = false;
+    	},
+    	error: (error) => {
+    		this.message(error, 'error');
+    		this.loadingPages = false;
+    	}
+    });
+  }
+
+  slugToLabel(_s: string) {
+  	return _s.replace(/_/gi, " ");
+  }
+
+
+  /*
+  *	Custom snackbar message
+  */
+  message(msg: string, panelClass: string = '', verticalPosition: any = undefined) {
+    this._snackBar.open(msg, 'X', {
+      duration: 8000,
+      horizontalPosition: 'center',
+      verticalPosition: verticalPosition,
+      panelClass: panelClass
+    });
+  }
+
+
+  ngOnDestroy() {
+  	if (this.pagesSubscription) {
+  		this.pagesSubscription.unsubscribe();
+  	}
+
+  }
+
+
+  async getArverifyVerification(_address: string) {
+    const verification = await getVerification(_address);
+
+    return ({
+      verified: verification.verified,
+      icon: verification.icon,
+      percentage: verification.percentage
+    });
   }
 
 }
--- src/app/core/arwiki-query.ts
+++ src/app/core/arwiki-query.ts
@@ -7,6 +7,7 @@ import { ArwikiSettingsContract } from '../arwiki-contracts/arwiki-settings';
 export class ArwikiQuery {
 	private _ardb: ArDB;
 	private _arweave: any;
+  private _addressesValidated: any;
 
 	constructor(_arweave: any) {
 		this._ardb = new ArDB(_arweave);
@@ -187,6 +188,89 @@ export class ArwikiQuery {
     return res;
   }
 
+  /*
+  * @dev
+  */
+  getPagesByCategory(
+    _category: string,
+    _settingsContract: ArwikiSettingsContract
+  ) {
+    return _settingsContract.getState(this._arweave)
+      .pipe(
+        switchMap((settingsContractState) => {
+          return this.getVerifiedPagesByCategories(
+            settingsContractState.adminList,
+            [_category]
+          );
+        }),
+        switchMap((verifiedPages) => {
+          const verifiedPagesList = [];
+          for (let p of verifiedPages) {
+            const vrfdPageId = this.searchKeyNameInTags(p.node.tags, 'Arwiki-Page-Id');
+            verifiedPagesList.push(vrfdPageId);
+          }
+
+          return this.getTXsData(verifiedPagesList);
+        }),
+        switchMap((txs) => {
+          const finalRes: any = [];
+          for (let p of txs) {
+            const title = this.searchKeyNameInTags(p.node.tags, 'Arwiki-Page-Title');
+            const slug = this.searchKeyNameInTags(p.node.tags, 'Arwiki-Page-Slug');
+            const category = this.searchKeyNameInTags(p.node.tags, 'Arwiki-Page-Category');
+            const img = this.searchKeyNameInTags(p.node.tags, 'Arwiki-Page-Img');
+            const owner = p.node.owner.address;
+            const id = p.node.id;
+            
+            finalRes.push({
+              title: title,
+              slug: slug,
+              category: category,
+              img: img,
+              owner: owner,
+              id: id
+            });
+            
+          }
+          return of(finalRes);
+        })
+      );
+  }
+
+  /*
+  * @dev
+  */
+  getVerifiedPagesByCategories(owners: string[], categories: string[]): Observable<any> {
+    const tags = [
+      {
+        name: 'Service',
+        values: ['ArWiki'],
+      },
+      {
+        name: 'Arwiki-Type',
+        values: ['Validation'],
+      },
+      {
+        name: 'Arwiki-Page-Category',
+        values: categories
+      }
+    ];
+
+    const obs = new Observable((subscriber) => {
+      this._ardb!.search('transactions')
+        .limit(100)
+        .from(owners)
+        .tags(tags).find().then((res) => {
+          subscriber.next(res);
+          subscriber.complete();
+        })
+        .catch((error) => {
+          subscriber.error(error);
+        });
+
+    });
+    return obs;
+  }
 
 
 
