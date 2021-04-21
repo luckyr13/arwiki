bea7f4a00f3458f712ca5d4f0733a4616c8cd17e
technodromex <ricardo.guzmantorres@owasp.org>
2021-04-21 18:00:43 -0500

    Update page preview component

--- src/app/page/preview/preview.component.ts
+++ src/app/page/preview/preview.component.ts
@@ -9,6 +9,7 @@ import { ArweaveService } from '../../core/arweave.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { Location } from '@angular/common';
+import { ArwikiQuery } from '../../core/arwiki-query';
 
 @Component({
   templateUrl: './preview.component.html',
@@ -17,7 +18,10 @@ import { Location } from '@angular/common';
 export class PreviewComponent implements OnInit, OnDestroy {
 	htmlContent: string = '';
 	pageSubscription: Subscription = Subscription.EMPTY;
+  page: any;
   loadingPage: boolean = false;
+  arwikiQuery: ArwikiQuery|null = null;
+  baseURL: string = this._arweave.baseURL;
 
   constructor(
     private route: ActivatedRoute,
@@ -27,30 +31,15 @@ export class PreviewComponent implements OnInit, OnDestroy {
   ) { }
 
   ngOnInit(): void {
-  	const contractAddress = this.route.snapshot.paramMap.get('id');
-    this.loadingPage = true;
-
-  	this._arweave.getDataAsString(contractAddress!)
-  		.then((data) => {
-				this.htmlContent = this.markdownToHTML(data);
-        this.loadingPage = false;
-			}).catch((error) => {
-				this.message(error, 'error');
-        this.loadingPage = false;
-			});
+  	const contractAddress = this.route.snapshot.paramMap.get('id')!;
+    this.arwikiQuery = new ArwikiQuery(this._arweave.arweave);
+    this.loadPageTXData(contractAddress);
 
     this.route.paramMap.subscribe(params => {
       const pageId = params.get('id');
       
       if (pageId) {
-        this._arweave.getDataAsString(pageId)
-          .then((data) => {
-            this.htmlContent = this.markdownToHTML(data);
-            this.loadingPage = false;
-          }).catch((error) => {
-            this.message(error, 'error');
-            this.loadingPage = false;
-          });
+        this.loadPageTXData(contractAddress);
       }
 
     });
@@ -58,7 +47,47 @@ export class PreviewComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-  	
+  	if (this.pageSubscription) {
+      this.pageSubscription.unsubscribe();
+    }
+  }
+
+  loadPageTXData(contractAddress: string) {
+    this.loadingPage = true;
+    this.pageSubscription = this.arwikiQuery!.getTXsData([contractAddress]).subscribe({
+      next: (txData) => {
+        if (txData && txData.length) {
+          const p = txData[0];
+          this.page = {
+            id: p.node.id,
+            title: this.searchKeyNameInTags(p.node.tags, 'Arwiki-Page-Title'),
+            slug: this.searchKeyNameInTags(p.node.tags, 'Arwiki-Page-Slug'),
+            category: this.searchKeyNameInTags(p.node.tags, 'Arwiki-Page-Category'),
+            language: this.searchKeyNameInTags(p.node.tags, 'Arwiki-Page-Lang'),
+            img: this.searchKeyNameInTags(p.node.tags, 'Arwiki-Page-Img'),
+            owner: p.node.owner.address,
+            block: p.node.block
+          };
+          // Load content
+          this.loadPageData(contractAddress);
+
+        }
+      },
+      error: (error) => {
+        this.message(error, 'error');
+      }
+    });
+  }
+
+  loadPageData(contractAddress: string) {
+    this._arweave.getDataAsString(contractAddress!)
+      .then((data) => {
+        this.htmlContent = this.markdownToHTML(data);
+        this.loadingPage = false;
+      }).catch((error) => {
+        this.message(error, 'error');
+        this.loadingPage = false;
+      });
   }
 
   markdownToHTML(_markdown: string) {
@@ -82,5 +111,16 @@ export class PreviewComponent implements OnInit, OnDestroy {
 
   goBack() {
     this._location.back();
+  }
+
+
+  searchKeyNameInTags(_arr: any[], _key: string) {
+    let res = '';
+    for (const a of _arr) {
+      if (a.name.toUpperCase() === _key.toUpperCase()) {
+        return a.value;
+      }
+    }
+    return res;
   }
 }
