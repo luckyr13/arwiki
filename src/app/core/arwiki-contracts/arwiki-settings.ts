43c04764a24ea668d97d4c2f2cf487b93fbd0acf
technodromex <ricardo.guzmantorres@owasp.org>
2021-05-10 15:39:18 -0500

    Store the contract state in a private variable

--- src/app/core/arwiki-contracts/arwiki-settings.ts
+++ src/app/core/arwiki-contracts/arwiki-settings.ts
@@ -14,25 +14,33 @@ import { ArwikiAdminList } from '../interfaces/arwiki-admin-list';
 export class ArwikiSettingsContract
 {
 	private _contractAddress: string = 'LWou280IGIbtK58-FYKIhBklXLt9rtXpu3dJzhNRrw8';
+	private _state: ArwikiSettings|null = null;
 
 	constructor(private _arweave: ArweaveService) {
 	}
+
 
 	/*
 	*	@dev Get full contract state as Observable
 	*/
 	getState(): Observable<ArwikiSettings> {
 		const obs = new Observable<ArwikiSettings>((subscriber) => {
-			readContract(this._arweave.arweave, this._contractAddress)
-				.then((state: ArwikiSettings) => {
-					subscriber.next(state);
-					subscriber.complete();
-				}).catch((error) => {
-					subscriber.error(error);
-				});
-
+			if (this._state) {
+				// Return local copy
+				subscriber.next(this._state);
+				subscriber.complete();
+			} else {
+				// Get state from contract
+				readContract(this._arweave.arweave, this._contractAddress)
+					.then((state: ArwikiSettings) => {
+						this._state = state;
+						subscriber.next(state);
+						subscriber.complete();
+					}).catch((error) => {
+						subscriber.error(error);
+					});
+			}
 		});
-
 		return obs;
 	}
 
