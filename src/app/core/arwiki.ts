e6ad9e08b456cc4cd842adc0c9dbd6c9a3285396
technodromex <ricardo.guzmantorres@owasp.org>
2021-05-12 19:22:18 -0500

    Add new methods (createMainPageTX and createDeleteTX)

--- src/app/core/arwiki.ts
+++ src/app/core/arwiki.ts
@@ -18,7 +18,7 @@ export class Arwiki {
 		this._arweave = _arweave;
 	}
 
-	/*
+  /*
   * @dev All pages needs to be validated first 
   * to be listed on the Arwiki. Validations are special TXs
   * with custom tags (Arwiki-Type: Validation)
@@ -47,13 +47,13 @@ export class Arwiki {
     await this._arweave.transactions.post(tx)
     return tx.id;
   }
-	
-	/*
-	*	@dev Create an Arwiki Tag
-	* A tag in the arwiki context is a TX with custom "tags"
-	* that helps to the search engine to locate pages
-	* (Arwiki-Type: Tag)
-	*/
+  
+  /*
+  *  @dev Create an Arwiki Tag
+  * A tag in the arwiki context is a TX with custom "tags"
+  * that helps to the search engine to locate pages
+  * (Arwiki-Type: Tag)
+  */
   async createTagTXForArwikiPage(
     _pageId: string,
     _tag: string,
@@ -80,6 +80,64 @@ export class Arwiki {
     return tx.id;
   }
 
+  /*
+  * @dev Pages can be removed
+  * if an admin creates a Delete TX (Arwiki-Type: DeletePage)
+  */
+  async createDeleteTXForArwikiPage(
+    _pageId: string,
+    _slug: string,
+    _category: string,
+    _langCode: string,
+    _privateKey: any
+  ) {
+    const jwk = _privateKey;
+    const data = { pageId: _pageId, slug: _slug, category: _category };
+    const tx = await this._arweave.createTransaction({
+      data: JSON.stringify(data)
+    }, jwk);
+    tx.addTag('Content-Type', 'text/json');
+    tx.addTag('Service', 'ArWiki');
+    tx.addTag('Arwiki-Type', 'DeletePage');
+    tx.addTag('Arwiki-Page-Id', _pageId);
+    tx.addTag('Arwiki-Page-Slug', _slug);
+    tx.addTag('Arwiki-Page-Category', _category);
+    tx.addTag('Arwiki-Page-Lang', _langCode);
+    tx.addTag('Arwiki-Version', arwikiVersion[0]);
+    await this._arweave.transactions.sign(tx, jwk)
+    await this._arweave.transactions.post(tx)
+    return tx.id;
+  }
+
+  /*
+  * @dev A page can be set as the Main Page
+  * if an admin creates a MainPage TX (Arwiki-Type: MainPage)
+  * Note: The latest MainPage TX would be the considered as the main page
+  */
+  async createMainPageTXForArwikiPage(
+    _pageId: string,
+    _slug: string,
+    _category: string,
+    _langCode: string,
+    _privateKey: any
+  ) {
+    const jwk = _privateKey;
+    const data = { pageId: _pageId, slug: _slug, category: _category };
+    const tx = await this._arweave.createTransaction({
+      data: JSON.stringify(data)
+    }, jwk);
+    tx.addTag('Content-Type', 'text/json');
+    tx.addTag('Service', 'ArWiki');
+    tx.addTag('Arwiki-Type', 'MainPage');
+    tx.addTag('Arwiki-Page-Id', _pageId);
+    tx.addTag('Arwiki-Page-Slug', _slug);
+    tx.addTag('Arwiki-Page-Category', _category);
+    tx.addTag('Arwiki-Page-Lang', _langCode);
+    tx.addTag('Arwiki-Version', arwikiVersion[0]);
+    await this._arweave.transactions.sign(tx, jwk)
+    await this._arweave.transactions.post(tx)
+    return tx.id;
+  }
 
 
 }
