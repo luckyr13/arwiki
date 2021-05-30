c4991f3971d5db1aa6cfaa4c1b7c028e2deffa1b
technodromex <ricardo.guzmantorres@owasp.org>
2021-05-30 01:06:03 -0500

    Update arwiki version (this removes all pages on previous versions)

--- src/app/core/arwiki.ts
+++ src/app/core/arwiki.ts
@@ -5,7 +5,7 @@ import { ArwikiPage } from './interfaces/arwiki-page';
 * Arwiki versions supported by the system
 * arwikiVersion[0] the first position must contain the latest supported version
 */
-export const arwikiVersion = ['0.4'];
+export const arwikiVersion = ['0.5'];
 
 /*
 *	@dev Main class
