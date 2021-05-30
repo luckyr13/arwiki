cff816b2be61de9cb45142fd9ffb270a2be091a5
technodromex <ricardo.guzmantorres@owasp.org>
2021-05-29 23:50:00 -0500

    Update dashboard component (use balance card component)

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import { ArweaveService } from '../core/arweave.service';
 import { Observable, Subscription, EMPTY } from 'rxjs';
@@ -7,6 +7,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { UserSettingsService } from '../core/user-settings.service';
 import { ArwikiQuery } from '../core/arwiki-query';
 import { Location } from '@angular/common';
+import { ArwikiTokenContract } from '../core/arwiki-contracts/arwiki-token';
 declare const window: any;
 
 @Component({
@@ -14,10 +15,14 @@ declare const window: any;
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
 	mainAddress: string = this._auth.getMainAddressSnapshot();
-	balance: Observable<string> = this._arweave.getAccountBalance(this.mainAddress);
-	
+	balance: string = '';
+	balancePST: string = '';
+  balanceSubscription: Subscription = Subscription.EMPTY;
+  balancePSTSubscription: Subscription = Subscription.EMPTY;
+  loadingBalance: boolean = false;
+  loadingBalancePST: boolean = false;
   loading: boolean = false;
   txmessage: string = '';
   lastTransactionID: Observable<string> = this._arweave.getLastTransactionID(
@@ -29,11 +34,48 @@ export class DashboardComponent implements OnInit {
   	private _arweave: ArweaveService,
     private _auth: AuthService,
     private _userSettings: UserSettingsService,
-    private _location: Location
+    private _location: Location,
+    private _arwikiTokenContract: ArwikiTokenContract
   ) { }
 
   ngOnInit() {
+    this.loadingBalance = true;
+    this.balanceSubscription = this._arweave
+      .getAccountBalance(this.mainAddress)
+      .subscribe({
+        next: (res: string) => {
+          this.balance = res;
+          this.loadingBalance = false;
+        },
+        error: (error) => {
+          this.message(error, 'error');
+          this.loadingBalance = false;
+        }
+      });
 
+    this.loadingBalancePST = true;
+    this.balancePSTSubscription = this._arwikiTokenContract
+      .getBalance(this.mainAddress)
+      .subscribe({
+        next: (res: string) => {
+          this.balancePST = res;
+          this.loadingBalancePST = false;
+        },
+        error: (error) => {
+          this.message(error, 'error');
+          this.loadingBalancePST = false;
+        }
+      });
+
+  }
+
+  ngOnDestroy() {
+    if (this.balanceSubscription) {
+      this.balanceSubscription.unsubscribe();
+    }
+    if (this.balancePSTSubscription) {
+      this.balancePSTSubscription.unsubscribe();
+    }
   }
 
   /*
