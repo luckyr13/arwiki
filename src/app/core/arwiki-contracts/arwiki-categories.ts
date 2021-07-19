08021c07cc501780638f34f36d983adb9043ca6a
technodromex <ricardo.guzmantorres@owasp.org>
2021-07-19 18:46:53 -0500

    Probably we dont need contracts for categories and langs

--- src/app/core/arwiki-contracts/arwiki-categories.ts
+++ src/app/core/arwiki-contracts/arwiki-categories.ts
@@ -12,15 +12,64 @@ import { ArwikiCategoryIndex } from '../interfaces/arwiki-category-index';
 })
 export class ArwikiCategoriesContract
 {
-	private _contractAddress: string = 'b7EcqPqcHg1w2z3dlGpAp7qQVFhcdK_fDiiY1XpX5Rc';
+	private _contractAddress: string = 'P2S3XXexvWQ7hblD5yzB3eM1-VK8YYuQj3GdkI-vfeg';
 	private _state: ArwikiCategoryIndex = {};
+	private _localState: ArwikiCategoryIndex = {
+		"the_arweave_project": {
+			"slug": "the_arweave_project",
+			"label": "The Arweave Project",
+			"order": 10,
+			"active": true
+		},
+		"the_arweave_protocol": {
+			"slug": "the_arweave_protocol",
+			"label": "The Arweave Protocol",
+			"order": 20,
+			"active": true
+		},
+		"profit_sharing": {
+			"slug": "profit_sharing",
+			"label": "Profit Sharing",
+			"order": 30,
+			"active": true
+		},
+		"mining": {
+			"slug": "mining",
+			"label": "Mining",
+			"order": 40,
+			"active": true
+		},
+		"building_on_arweave": {
+			"slug": "building_on_arweave",
+			"label": "Building on Arweave",
+			"order": 50,
+			"active": true
+		},
+		"resources": {
+			"slug": "resources",
+			"label": "Resources",
+			"order": 100,
+			"active": true
+		}
+	};
 
 	constructor(private _arweave: ArweaveService) {
 	}
+
+	getState(): Observable<ArwikiCategoryIndex> {
+		const obs = new Observable<ArwikiCategoryIndex>((subscriber) => {
+			this._state = this._localState;
+			subscriber.next(this._localState);
+			subscriber.complete();
+		});
+
+		return obs;
+	}
+
 	/*
 	*	@dev Get full contract state as Observable
 	*/
-	getState(): Observable<ArwikiCategoryIndex> {
+	getState_old(): Observable<ArwikiCategoryIndex> {
 		const obs = new Observable<ArwikiCategoryIndex>((subscriber) => {
 			if (Object.keys(this._state).length > 0) {
 				subscriber.next(this._state);
--- src/app/core/arwiki-contracts/arwiki-lang-index.ts
+++ src/app/core/arwiki-contracts/arwiki-lang-index.ts
@@ -14,15 +14,98 @@ export class ArwikiLangIndexContract
 {
 	private _langs: ArwikiLangIndex = {};
 	private _contractAddress: string = '-NZMPdQJd2P3N9pXzKh8mV_PljLzmYtjFnVRZ9uoUpM';
+	private _localLangs: ArwikiLangIndex = {
+	"en": {
+		"code": "en",
+		"iso_name": "English",
+		"native_name": "English",
+		"writing_system": "LTR",
+		"active": true
+	},
+	"es": {
+		"code": "es",
+		"iso_name": "Spanish",
+		"native_name": "Español",
+		"writing_system": "LTR",
+		"active": true
+	},
+	"de": {
+		"code": "de",
+		"iso_name": "German",
+		"native_name": "Deutsch",
+		"writing_system": "LTR",
+		"active": true
+	},
+	"hi": {
+		"code": "hi",
+		"iso_name": "Hindi",
+		"native_name": "हिन्दी, हिंदी",
+		"writing_system": "LTR",
+		"active": true
+	},
+	"ar": {
+		"code": "ar",
+		"iso_name": "Arabic",
+		"native_name": "العربية",
+		"writing_system": "RTL",
+		"active": true
+	},
+	"he": {
+		"code": "he",
+		"iso_name": "Hebrew",
+		"native_name": "עברית",
+		"writing_system": "RTL",
+		"active": true
+	},
+	"hu": {
+		"code": "hu",
+		"iso_name": "Hungarian",
+		"native_name": "magyar",
+		"writing_system": "LTR",
+		"active": true
+	},
+	"ga": {
+		"code": "ga",
+		"iso_name": "Irish",
+		"native_name": "Gaeilge",
+		"writing_system": "LTR",
+		"active": true
+	},
+	"zh": {
+		"code": "zh",
+		"iso_name": "Chinese",
+		"native_name": "中文",
+		"writing_system": "LTR",
+		"active": true
+	},
+	"id": {
+		"code": "id",
+		"iso_name": "Indonesian",
+		"native_name": "Bahasa Indonesia",
+		"writing_system": "LTR",
+		"active": true
+	}
+};
+
 
 
 	constructor(private _arweave: ArweaveService) {
-
 	}
+
+	getState(): Observable<ArwikiLangIndex> {
+		const obs = new Observable<ArwikiLangIndex>((subscriber) => {
+			this._langs = this._localLangs;
+			subscriber.next(this._localLangs);
+			subscriber.complete();
+		});
+
+		return obs;
+	}
+
 	/*
 	*	@dev Get full contract state as Observable
 	*/
-	getState(): Observable<ArwikiLangIndex> {
+	getState_old(): Observable<ArwikiLangIndex> {
 		const obs = new Observable<ArwikiLangIndex>((subscriber) => {
 			if (Object.keys(this._langs).length > 0) {
 				subscriber.next(this._langs);
