cec57e2957e5d95fa71003b8f4a8fb7f8990b0bb
technodromex <richie.isc@gmail.com>
2022-01-14 19:30:39 -0600

    Update libs and fix dependencies issues

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -47,13 +47,6 @@
  */
 import 'zone.js';  // Included with Angular CLI.
 
-
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-(window as any)['global'] = window;
-(window as any).process = {
-  env: {
-      NODE_ENV: ''
-  }
-}
