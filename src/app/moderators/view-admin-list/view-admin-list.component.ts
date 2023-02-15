df7ba6c2d709cb59d628331c76369d02b3e581b2
luckyr13 <richie.isc@gmail.com>
2023-02-14 21:41:00 -0600

    Remove add-admin from Moderator's menu

--- src/app/moderators/view-admin-list/view-admin-list.component.ts
+++ src/app/moderators/view-admin-list/view-admin-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-admin-list',
@@ -8,10 +9,17 @@ import { AuthService } from '../../auth/auth.service';
 })
 export class ViewAdminListComponent implements OnInit {
 	adminList = this._auth.getAdminList();
+  routerLang: string = '';
 
-  constructor(private _auth: AuthService) { }
+  constructor(
+    private _auth: AuthService,
+    private _route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // Get language from route
+    this._route.paramMap.subscribe((params) => {
+      this.routerLang = params.get('lang')!;
+    });
   }
 
 }
