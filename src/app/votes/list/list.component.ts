7b7f65414f6c4199f78ee427b739cb5c2f1079cf
luckyr13 <richie.isc@gmail.com>
2023-02-14 22:39:17 -0600

    Add current height and load contract settings

--- src/app/votes/list/list.component.ts
+++ src/app/votes/list/list.component.ts
@@ -5,6 +5,8 @@ import {
 } from '../../core/arwiki-contracts/arwiki-token.service';
 import { Subscription } from 'rxjs';
 import { ArwikiVote } from '../../core/interfaces/arwiki-vote';
+import { ArweaveService } from '../../core/arweave.service';
+import {switchMap} from 'rxjs';
 
 @Component({
   selector: 'app-list',
@@ -15,25 +17,37 @@ export class ListComponent implements OnInit {
   loading = false;
   getVotesSubscription = Subscription.EMPTY;
   votes: ArwikiVote[] = [];
+  currentBlockHeight = 0;
+  voteLength = 0;
 
   constructor(
     private _location: Location,
-    private _tokenContract: ArwikiTokenContract) {
+    private _tokenContract: ArwikiTokenContract,
+    private _arweave: ArweaveService) {
 
   }
 
   ngOnInit() {
     this.loading = true;
-    this.getVotesSubscription = this._tokenContract.getVotes()
-      .subscribe({
+    this.getVotesSubscription = this._arweave.getNetworkInfo().pipe(
+      switchMap((network) => {
+        this.currentBlockHeight = network.height;
+        return this._tokenContract.getSettings();
+      }),
+      switchMap((settings) => {
+        this.voteLength = settings.get('voteLength') || 0;
+        return this._tokenContract.getVotes();
+      })
+    ).subscribe({
         next: (res: ArwikiVote[]) => {
-          this.votes = res || [];
-          this.votes.reverse();
+          const votes = res || [];
+          votes.reverse();
+          this.votes = votes;
           this.loading = false;
 
         },
         error: (err) => {
-          console.error('Votes', err);
+          console.error('Votes Error', err);
           this.loading = false;
         }
       })
@@ -51,6 +65,10 @@ export class ListComponent implements OnInit {
       return 0;
     }
     return (yayOrNay * 100) / totalVotes;
+  }
+
+  formatBlocks(block: number) {
+    return this._arweave.formatBlocks(block);
   }
 
 }
