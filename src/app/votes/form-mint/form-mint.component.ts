9b3bac210a450639a334bd07938d249cf4149822
luckyr13 <richie.isc@gmail.com>
2023-02-19 08:05:51 -0600

    Update form mint

--- src/app/votes/form-mint/form-mint.component.ts
+++ src/app/votes/form-mint/form-mint.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ArweaveService } from '../../core/arweave.service';
 
 @Component({
   selector: 'app-form-mint',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-mint.component.scss']
 })
 export class FormMintComponent {
+  mintForm = new FormGroup({
+    recipient: new FormControl(
+      '', [Validators.required, Validators.maxLength(43)]
+    ),
+    qty: new FormControl(1, [Validators.required, Validators.min(1)]),
+    lockLength: new FormControl(0, [Validators.required, Validators.min(0)]),
+  });
+  lockMinLength = 0;
+  lockMaxLength = 1;
 
+  constructor(private _arweave: ArweaveService) {
+
+  }
+
+  public get qty() {
+    return this.mintForm.get('qty');
+  }
+
+  public get lockLength() {
+    return this.mintForm.get('lockLength');
+  }
+
+
+  onSubmit() {
+    alert('test')
+
+  }
+
+  formatBlocks(len: number): string {
+    return this._arweave.formatBlocks(len);
+  }
 }
