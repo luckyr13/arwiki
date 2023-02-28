e0bbdefdece8810a32cb7ff73b7993edc828d875
luckyr13 <richie.isc@gmail.com>
2023-02-28 12:05:01 -0600

    Update Set Settings Vote form

--- src/app/votes/form-set-settings/form-set-settings.component.ts
+++ src/app/votes/form-set-settings/form-set-settings.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import {
+  Component, OnInit, OnDestroy,
+  Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ArweaveService } from '../../core/arweave.service';
 import { Subscription } from 'rxjs';
@@ -6,6 +8,15 @@ import {
   ArwikiTokenContract 
 } from '../../core/arwiki-contracts/arwiki-token.service';
 import { UtilsService } from '../../core/utils.service';
+import { 
+  ArwikiTokenVotesService 
+} from '../../core/arwiki-contracts/arwiki-token-votes.service';
+import { 
+  arwikiVersion 
+} from '../../core/arwiki';
+import { 
+  AuthService 
+} from '../../auth/auth.service';
 
 @Component({
   selector: 'app-form-set-settings',
@@ -13,36 +24,59 @@ import { UtilsService } from '../../core/utils.service';
   styleUrls: ['./form-set-settings.component.scss']
 })
 export class FormSetSettingsComponent implements OnInit, OnDestroy {
+  maxLengthNote = 200;
   settingsForm = new FormGroup({
     notes: new FormControl(
-      '', [Validators.required, Validators.maxLength(200)]
+      '', [Validators.required, Validators.maxLength(this.maxLengthNote)]
     )
   });
-  loading = false;
+  loadingSubmit = false;
+  @Output() working = new EventEmitter<boolean>();
+  tx = '';
+  error = '';
+  submitVoteSubscription = Subscription.EMPTY;
 
   constructor(
     private _arweave: ArweaveService,
     private _tokenContract: ArwikiTokenContract,
-    private _utils: UtilsService) {
+    private _utils: UtilsService,
+    private _tokenContractVotes: ArwikiTokenVotesService,
+    private _auth: AuthService) {
   }
 
+  public get notes() {
+    return this.settingsForm.get('notes')!;
+  }
 
   ngOnInit() {
-    this.loading = false;
     
   }
 
   ngOnDestroy() {
-
+    this.submitVoteSubscription.unsubscribe();
   }
 
-
   onSubmit() {
-    alert('test')
+    const note: string = this.notes.value!.trim();
+    this.disableForm(true);
 
+    const jwk = this._auth.getPrivateKey();
+ 
   }
 
   formatBlocks(len: number): string {
     return this._arweave.formatBlocks(len);
+  }
+
+  disableForm(disable: boolean) {
+    if (disable) {
+      this.notes.disable();
+      this.working.emit(true);
+      this.loadingSubmit = true;
+    } else {
+      this.notes.enable();
+      this.working.emit(false);
+      this.loadingSubmit = false;
+    }
   }
 }
